6609398ebe7949d3993efd6dd4fc3452ef0871d5
mkg0 <mehmetmorcay@gmail.com>
2020-08-02 13:23:15 +0000

    Deploying to gh-pages from  @ 2268bbce060093accc521957cc70854425a9ed18 🚀

---
+++ precache-manifest.806fc6688543b47851c402c174e29135.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc599ab8b34e72cc0839a976df2a7fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f58200fbac5b7d7e061",
+    "url": "/static/css/2.a55ddcfb.chunk.css"
+  },
+  {
+    "revision": "b2bcdc8eb3100d071ba3",
+    "url": "/static/css/main.0dbc6584.chunk.css"
+  },
+  {
+    "revision": "8f58200fbac5b7d7e061",
+    "url": "/static/js/2.a8cdc777.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.a8cdc777.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2bcdc8eb3100d071ba3",
+    "url": "/static/js/main.1ede30a8.chunk.js"
+  },
+  {
+    "revision": "4f7e056099c3de68cb3b",
+    "url": "/static/js/runtime-main.87a03c34.js"
+  },
+  {
+    "revision": "f55dba4930e32173e499e2eeafdab5eb",
+    "url": "/static/media/sunny.f55dba49.svg"
+  }
+]);
